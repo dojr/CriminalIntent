7b5e2869eb1083f220b04ef5dbfbf3ccc11b4955
donaldjr <hamblindj@gmail.com>
2018-10-14 23:24:20 -0700

    finished ch 7 and 8 criminal stuff

--- app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeListFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -41,7 +42,7 @@ public class CrimeListFragment extends Fragment {
     }
 
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Crime mCrime;
 
         public void bind(Crime crime) {
@@ -51,9 +52,16 @@ public class CrimeListFragment extends Fragment {
         }
         public CrimeHolder (LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
+                    .show();
         }
     }
 
