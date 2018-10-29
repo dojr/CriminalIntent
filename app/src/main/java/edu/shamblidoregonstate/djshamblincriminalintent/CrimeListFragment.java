81737f4a307687f5808afc559574cbf399ac097b
donaldjr <hamblindj@gmail.com>
2018-10-29 01:06:58 -0700

    add image view

--- app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
     private TextView mTitleTextView;
     private TextView mDateTextView;
+    private ImageView mImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
