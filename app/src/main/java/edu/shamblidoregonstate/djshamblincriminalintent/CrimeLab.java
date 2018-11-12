68cdc00f3884a79ab3935963b93aba0b8f547c87
DJ <hamblindj@gmail.com>
2018-11-11 21:11:02 -0800

    add database schema and helper and see it in the device file explorer:

--- app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeLab.java
+++ app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeLab.java
@@ -1,14 +1,19 @@
 package edu.shamblidoregonstate.djshamblincriminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import edu.shamblidoregonstate.djshamblincriminalintent.database.CrimeBaseHelper;
+
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -18,7 +23,11 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
+
     }
 
     public List<Crime> getCrimes() {
