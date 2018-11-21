66bac7322e34c1934c0bb3ca552729ada863a766
DJ <hamblindj@gmail.com>
2018-11-21 07:37:57 -0800

    finish chatper 16 camera

--- app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeLab.java
+++ app/src/main/java/edu/shamblidoregonstate/djshamblincriminalintent/CrimeLab.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -97,4 +98,10 @@ public class CrimeLab {
                 null // orderBy
         );
         return new CrimeCursorWrapper(cursor);    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
+    }
+
 }
